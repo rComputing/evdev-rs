2aff02e60cd78964d6d63d9902694a503ee5dcad
Travis CI User <travis@example.org>
2019-02-21 14:57:38 +0000

    Documentation for ndesh26/evdev-rs@d9464bf

---
+++ implementors/core/ops/drop/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["evdev_rs"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"evdev_rs/struct.Device.html\" title=\"struct evdev_rs::Device\">Device</a>",synthetic:false,types:["evdev_rs::Device"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex['evdev_rs'] = {"name":"","dirs":[],"files":["enums.rs","lib.rs","logging.rs","macros.rs","util.rs"]};
+sourcesIndex['evdev_sys'] = {"name":"","dirs":[],"files":["lib.rs"]};
